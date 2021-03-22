d7a98ce3b8cf0f7356ee19c048b93a253677bb96
Fernando Tinoco <FERNANDOTINOCO_45@hotmail.com>
2021-03-22 09:01:56 -0600

    Adjust main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,12 @@
 // Add your javascript here
 // Don't forget to add it into respective layouts where this js file is needed
 
-$(document).ready(function() {
-  AOS.init( {
+$(document).ready(function () {
+  AOS.init({
     // uncomment below for on-scroll animations to played only once
     // once: true  
   }); // initialize animate on scroll library
-  $('#frmContact').on('submit', function(evt){
+  $('#frmContact').on('submit', function (evt) {
     evt.preventDefault();
     let data = {
       message: $('[name="message"]').val(),
@@ -21,7 +21,7 @@ $(document).ready(function() {
       dataType: 'json',
       contentType: 'application/json',
       success: function (res) {
-        if(res === 'sent'){
+        if (res === 'sent') {
           Swal.fire({
             icon: 'success',
             title: 'Thanks for contact me...',
@@ -35,40 +35,40 @@ $(document).ready(function() {
           });
         }
       }
-  }); 
-
+    });
+  });
 });
 
 // Smooth scroll for links with hashes
 $('a.smooth-scroll')
-.click(function(event) {
-  // On-page links
-  if (
-    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-    && 
-    location.hostname == this.hostname
-  ) {
-    // Figure out element to scroll to
-    var target = $(this.hash);
-    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    // Does a scroll target exist?
-    if (target.length) {
-      // Only prevent default if animation is actually gonna happen
-      event.preventDefault();
-      $('html, body').animate({
-        scrollTop: target.offset().top
-      }, 1000, function() {
-        // Callback after animation
-        // Must change focus!
-        var $target = $(target);
-        $target.focus();
-        if ($target.is(":focus")) { // Checking if the target was focused
-          return false;
-        } else {
-          $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-          $target.focus(); // Set focus again
-        };
-      });
+  .click(function (event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      &&
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function () {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
     }
-  }
-});
+  });
