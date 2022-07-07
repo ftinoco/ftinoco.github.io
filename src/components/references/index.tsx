5f0835b09a78b56b24c6b9fa54c18db1bad3329c
Fernando Tinoco <Fernando.Tinoco@stepstone.com>
2022-07-07 11:13:58 -0600

    Loading dynamically references data

--- src/components/references/index.tsx
+++ src/components/references/index.tsx
@@ -1,4 +1,16 @@
+import { useObservableState, useSubscription } from "observable-hooks";
+import { getReferenceData$, referenceData$ } from "../../services/reference-service";
+import { IReference } from "./interfaces";
+
 export const Reference = () => {
+    const references = useObservableState<IReference[]>(
+        referenceData$, []
+    );
+    useSubscription(getReferenceData$, (r) => { referenceData$.next(r) });
+
+    const getActiveClass = (index: number) => {
+        return index === 0 ? "active" : "";
+    }
     return (
         <div className="section" id="reference">
             <div className="container cc-reference">
@@ -6,70 +18,35 @@ export const Reference = () => {
                 <div className="card" data-aos="zoom-in">
                     <div className="carousel slide" id="cc-Indicators" data-ride="carousel">
                         <ol className="carousel-indicators">
-                            <li className="active" data-target="#cc-Indicators" data-slide-to="0"></li>
-                            <li data-target="#cc-Indicators" data-slide-to="1"></li>
-                            <li data-target="#cc-Indicators" data-slide-to="2"></li>
+                            {
+                                references.map((reference, idx) =>
+                                    <li key={idx}
+                                        className={getActiveClass(idx)}
+                                        data-target="#cc-Indicators"
+                                        data-slide-to={idx}>
+                                    </li>
+                                )
+                            }
                         </ol>
                         <div className="carousel-inner">
-                            <div className="carousel-item active">
-                                <div className="row">
-                                    <div className="col-lg-2 col-md-3 cc-reference-header">
-                                        <div className="avatar">
-                                            <div className="avatar__letters">JR</div>
+                            {
+                                references.map((reference, idx) =>
+                                    <div key={idx} className={`carousel-item ${getActiveClass(idx)}`}>
+                                        <div className="row">
+                                            <div className="col-lg-2 col-md-3 cc-reference-header">
+                                                <div className="avatar">
+                                                    <div className="avatar__letters">{reference.img}</div>
+                                                </div>
+                                                <div className="h5 pt-2">{reference.name}</div>
+                                                <p className="category">{reference.position}</p>                                                
+                                            </div>
+                                            <div className="col-lg-10 col-md-9">
+                                                {reference.comments.map((comment, i) => <p key={i}>{comment.description}</p>)}
+                                            </div>
                                         </div>
-                                        <div className="h5 pt-2">Jorge Rivera</div>
-                                        <p className="category">AWS Certified | Scrum Certified | SQL Server | .NET C# |
-                                            Consultant</p>
-                                    </div>
-                                    <div className="col-lg-10 col-md-9">
-                                        <p> Fernando is a dedicated and proactive professional. During the time we
-                                            shared in VersaTec, he worked with ASP.NET, C# and SQL Server in
-                                            different projects, and he proved to improve and grow in knowledge and
-                                            skills.</p>
-                                        <p> I have no doubt that any company that decides to acquire his services is
-                                            winning a great professional who will help it to achieve its goals.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="carousel-item">
-                                <div className="row">
-                                    <div className="col-lg-2 col-md-3 cc-reference-header">
-                                        <div className="avatar">
-                                            <div className="avatar__letters">WP</div>
-                                        </div>
-                                        <div className="h5 pt-2">William Palacios</div>
-                                        <p className="category">Software Engineer at PayPal</p>
-                                    </div>
-                                    <div className="col-lg-10 col-md-9">
-                                        <p> Fernando is one of the best professionals and .NET developers I have
-                                            worked with, he has outstanding leadership skills, his passion for
-                                            excellence and continuous improvement, always lead him to apply the best
-                                            software development standards and practices.</p>
-                                        <p> I had the pleasure to work with Fernando a few years ago, and I was very
-                                            impressed with his technical quality and soft skills, definitely a great
-                                            team player. He has my full confidence and recommendation, I'm sure he
-                                            will be able to exceed the expectations of any team, and will be a
-                                            fundamental piece to achieve the goals of any organization.</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="carousel-item">
-                                <div className="row">
-                                    <div className="col-lg-2 col-md-3 cc-reference-header">
-                                        <div className="avatar">
-                                            <div className="avatar__letters">JG</div>
-                                        </div>
-                                        <div className="h5 pt-2">José Guevara</div>
-                                        <p className="category">Data Engineer | Power BI | Sql Server | .NET C# | IT
-                                            Consultant</p>
-                                    </div>
-                                    <div className="col-lg-10 col-md-9">
-                                        <p> I have known Fernando for many years, we worked together and I know that
-                                            he is capable of performing as a developer with knowledge in ASP.NET, C#
-                                            AND MS SQL Server. He is a proactive and professional person.</p>
-                                    </div>
-                                </div>
-                            </div>
+                                    </div> 
+                                ) 
+                            } 
                         </div>
                     </div>
                 </div>
