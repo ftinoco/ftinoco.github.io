d066e91ba9a0a775885c8a8e6810eaa7d2a23a17
Fernando Tinoco <fernandotinoco_45@hotmail.com>
2022-10-06 00:02:57 -0600

    Relocating global consts

--- src/utils/consts/portfolio.ts
+++ src/utils/consts/portfolio.ts
@@ -1,7 +1,5 @@
 import { IPortfolio } from "../interfaces/interfaces";
 
-export const MAX_WORK_EXPERIENCE_TO_SHOW = 3;
-
 export const defaultValues: IPortfolio = {
     contact: {
         message: '',
