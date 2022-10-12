51e5c8b9801bb996f6ccffebb6b2ac46b64f1d35
Fernando Tinoco <fernandotinoco_45@hotmail.com>
2022-10-12 13:11:45 -0600

    Styling footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,18 +1,24 @@
 import { FC } from "react";
 import { IFooterProps } from "./interfaces";
+import './index.css';
 
 export const Footer: FC<IFooterProps> = ({ fullName, socialMedias }) => {
     return (
         <footer className="footer footer-default">
-            <div className="container text-center">
+            {/*   <div className="container text-center">
                 {socialMedias.map((sm, idx) =>
                     <a key={idx} className={`cc-${sm.icon.replace('fa-', '')} btn btn-link`}
                         target="_blank" href={sm.link} rel="noreferrer">
                         <i className={`fa ${sm.icon} fa-2x`} aria-hidden="true"></i>
                     </a>
                 )} 
+            </div> */}
+            <div className="footer-content">
+               {/*  <div className="h4 title text-center">{fullName}</div> */}
+                <div className="text-center">
+                    <p>© 2022 Resume. All Rights Reserverd</p>
+                </div>
             </div>
-            <div className="h4 title text-center">{fullName}</div>
         </footer>
     );
 }
