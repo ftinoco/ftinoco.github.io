3b7391a4f33bc0a13d54800c25492d6f7c38e3f0
Fernando Tinoco <fernandotinoco_45@hotmail.com>
2022-10-12 15:52:40 -0600

    Updating footer styles

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -5,18 +5,18 @@ import './index.css';
 export const Footer: FC<IFooterProps> = ({ fullName, socialMedias }) => {
     return (
         <footer className="footer footer-default">
-            {/*   <div className="container text-center">
-                {socialMedias.map((sm, idx) =>
-                    <a key={idx} className={`cc-${sm.icon.replace('fa-', '')} btn btn-link`}
-                        target="_blank" href={sm.link} rel="noreferrer">
-                        <i className={`fa ${sm.icon} fa-2x`} aria-hidden="true"></i>
-                    </a>
-                )} 
-            </div> */}
             <div className="footer-content">
-               {/*  <div className="h4 title text-center">{fullName}</div> */}
+                <div className="container text-center">
+                    {socialMedias.map((sm, idx) =>
+                        <a key={idx} className={`cc-${sm.icon.replace('fa-', '')} btn btn-link`}
+                            target="_blank" href={sm.link} rel="noreferrer">
+                            <i className={`fa ${sm.icon} fa-2x`} aria-hidden="true"></i>
+                        </a>
+                    )}
+                </div>
+                <div className="h4 title text-center">{fullName}</div>
                 <div className="text-center">
-                    <p>© 2022 Resume. All Rights Reserverd</p>
+                    <p>© 2022 Resume. All rights reserved.</p>
                 </div>
             </div>
         </footer>
