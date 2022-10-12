25ad14e5f50a9afc40112a9297f4282d761b231d
Fernando Tinoco <fernandotinoco_45@hotmail.com>
2022-10-12 15:36:08 -0600

    Updating design of contact form

--- src/components/contact/contact-form.tsx
+++ src/components/contact/contact-form.tsx
@@ -17,12 +17,12 @@ export const ContactForm = () => {
 
     const onSubmitHandler = async (info: IContact) => {
         MySwal.fire({
-            title: 'Sending...', 
+            title: 'Sending...',
             timerProgressBar: true,
             didOpen: () => {
                 MySwal.showLoading();
             },
-          })
+        })
         //MySwal.showLoading(MySwal.getDenyButton() as HTMLButtonElement);
         await sendEmail(info).then((result) => {
             MySwal.close();
@@ -44,16 +44,14 @@ export const ContactForm = () => {
                 });
                 console.debug(result.message);
             }
-        }); 
+        });
     }
- 
+
     return (
-        <div className="col-md-6">
+        <div className="contact-frm">
             <div className="card-body">
                 <form onSubmit={handleSubmit(onSubmitHandler)} autoComplete="off">
-                    <div className="p pb-3">
-                        <strong>Feel free to contact me</strong>
-                    </div>
+                    <div className="h4 title">Send a message</div>
                     <div className="row mb-3">
                         <div className="col">
                             <div className="input-group">
@@ -175,7 +173,7 @@ export const ContactForm = () => {
                         <div className="col">
                             <button type="submit"
                                 className="btn btn-primary" >
-                                Send
+                                SEND MESSAGE
                             </button>
                         </div>
                     </div>
--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -1,29 +1,38 @@
 import { ContactInfo } from "./contact-info";
 import { ContactForm } from "./contact-form";
 
+import './index.css';
+
 export const Contact = () => {
 
     return (
         <div className="section" id="contact">
-            <div className="cc-contact-information"
-                style={{
-                    backgroundImage: "url('images/staticmap.png')",
-                    backgroundRepeat: "no-repeat",
-                    backgroundSize: "cover"
-                }}>
+            <div className="cc-contact-information">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d124838.77655773303!2d-86.32850087786538!3d12.097650227236846!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f71560dd907880b%3A0x6c5ef4a2144f4c6a!2sManagua!5e0!3m2!1sen!2sni!4v1665608868740!5m2!1sen!2sni"
+                    width="600" height="450" style={{ border: 0 }}
+                    loading="lazy" className="background">
+                </iframe>
                 <div className="container">
                     <div className="cc-contact">
                         <div className="row justify-content-center">
                             <div className="col-md-10">
-                                <div className="card mb-0" data-aos="zoom-in">
-                                    <div className="h4 text-center title">Contact Me</div>
-                                    <div className="row">
-                                        <ContactForm />
+                            <div className="card mb-0" data-aos="zoom-in"> 
+                                    <div className="h2 title">GET IN TOUCH WITH ME</div> 
+                                <div className="row col-md-12">
+                                    <div className="col-md-4">
+                                        <div className="h4 title">Contact me</div>
+                                        <p>
+                                            Wheather you have an interesting project or just want to say hi, feel free to shoot me a message.
+                                        </p>
                                         <ContactInfo />
+                                    </div>
+                                    <div className="col-md-8">
+                                        <ContactForm />
                                     </div>
                                 </div>
                             </div>
                         </div>
+                            </div>
                     </div>
                 </div>
             </div>
