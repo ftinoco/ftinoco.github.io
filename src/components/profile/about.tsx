e8d1e420fa318fdbc3d4ad5260b7a09d088b75e7
Fernando Tinoco <fernandotinoco_45@hotmail.com>
2022-10-17 23:19:04 -0600

    Updating about section

--- src/components/profile/about.tsx
+++ src/components/profile/about.tsx
@@ -15,63 +15,74 @@ export const About: FC<{ profile: IProfile }> = ({ profile }) => {
     return (
         <div className="section" id="about">
             <div className="container">
+                <div className="h4 text-center mb-4 title">About me</div>
                 <div className="card" data-aos="fade-up" data-aos-offset="10">
-                    <div className="row">
-                        <div className="col-lg-6 col-md-12">
-                            <div className="card-body">
-                                <div className="h4 mt-0 title">About</div>
-                                {profile.about.map((txt, idx) => <p key={idx}>{txt.description}</p>)}
-                                <div className="row mt-3">
+                    <div className="card-body">
+                        <div className="row">
+                            <div className="col-md-12">
+                                <div className="card-body">
+                                    {profile.about.map((txt, idx) => <p key={idx}>{txt.description}</p>)}
+                                    {/*  <div className="row mt-3">
                                     <a className="btn btn-primary" href="/Resume.pdf" target="_blank" data-aos="zoom-in"
                                         data-aos-anchor="data-aos-anchor">Download CV</a>
+                                </div> */}
                                 </div>
                             </div>
                         </div>
-                        <div className="col-lg-6 col-md-12">
-                            <div className="card-body">
-                                <div className="h4 mt-0 title">Basic Information</div>
-                                <div className="row">
-                                    <div className="col-sm-4"><strong className="text-uppercase">Age:</strong></div>
-                                    <div className="col-sm-8" data-testid="age">{profile.age}</div>
-                                </div>
-                                <div className="row mt-3">
-                                    <div className="col-sm-4"><strong className="text-uppercase">Email:</strong></div>
-                                    <div className="col-sm-8">
-                                        <a href={"mailto:" + profile.emails.main}>
-                                            {profile.emails.main}
-                                        </a>
+                        <div className="row">
+                            <div className="col-lg-6 col-md-12">
+                                <div className="card-body">
+                                    <div className="h5 mt-0 title">Basic Information</div>
+                                    <div className="row">
+                                        <div className="col-sm-4"><strong className="text-uppercase">Age:</strong></div>
+                                        <div className="col-sm-8" data-testid="age">{profile.age}</div>
+                                    </div>
+                                    <div className="row mt-3">
+                                        <div className="col-sm-4"><strong className="text-uppercase">Email:</strong></div>
+                                        <div className="col-sm-8">
+                                            <a href={"mailto:" + profile.emails.main}>
+                                                {profile.emails.main}
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div className="row mt-3">
+                                        <div className="col-sm-4"><strong className="text-uppercase">Phone:</strong></div>
+                                        <div className="col-sm-8">{profile.phone}</div>
+                                    </div>
+                                    <div className="row mt-3">
+                                        <div className="col-sm-4"><strong className="text-uppercase">Address:</strong></div>
+                                        <div className="col-sm-8">{profile.address}</div>
+                                    </div>
+                                    <div className="row mt-3">
+                                        <div className="col-sm-4"><strong className="text-uppercase">Languages:</strong></div>
+                                        <div className="col-sm-8">
+                                            {
+                                                profile.languages.map((lan, idx) =>
+                                                    <span key={idx}
+                                                        style={{ marginBottom: "10px", display: 'block' }}>
+                                                        {lan.name} - {lan.level}
+                                                    </span>
+                                                )
+                                            }
+                                        </div>
                                     </div>
                                 </div>
-                                <div className="row mt-3">
-                                    <div className="col-sm-4"><strong className="text-uppercase">Phone:</strong></div>
-                                    <div className="col-sm-8">{profile.phone}</div>
+                            </div>
+                            <div className="col-lg-6 col-md-12">
+                                <div className="card-body">
+                                    <div className="h5 mt-0 title">My interests</div>
                                 </div>
-                                <div className="row mt-3">
-                                    <div className="col-sm-4"><strong className="text-uppercase">Address:</strong></div>
-                                    <div className="col-sm-8">{profile.address}</div>
-                                </div>
-                                <div className="row mt-3">
-                                    <div className="col-sm-4"><strong className="text-uppercase">Languages:</strong></div>
-                                    <div className="col-sm-8">
-                                        {
-                                            profile.languages.map((lan, idx) =>
-                                                <span key={idx}
-                                                    style={{ marginBottom: "10px", display: 'block' }}>
-                                                    {lan.name} - {lan.level}
-                                                </span>
-                                            )
-                                        }
-                                    </div>
-                                </div>
-                                <div className="row mt-3">
-                                    <a className="btn btn-primary smooth-scroll mr-2"
-                                        href="#contact"
-                                        onClick={(ev) => Utils.scrollTo(ev, setSection)}
-                                        data-aos="zoom-in"
-                                        data-aos-anchor="data-aos-anchor">
-                                        Hire Me
-                                    </a>
-                                </div>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-12 text-center">
+                                <a className="btn btn-outline-primary"
+                                    href="/Resume.pdf"
+                                    target="_blank"
+                                    data-aos="zoom-in"
+                                    data-aos-anchor="data-aos-anchor">
+                                    Download CV
+                                </a>
                             </div>
                         </div>
                     </div>
